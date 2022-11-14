288185b0fca862b95a5693677ad7c006a54bcf05
Steve Krenn <stevekrenn@gmail.com>
2022-11-13 19:57:57 -0600

    server machine switch

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express')
 const app = express()
 const PORT = process.env.port || 3000
 const db = require('./db/db.json')
+//Allows all notes to have a unique ID
 const { v4: uuidv4 } = require('uuid');
 
 //Allows public folder to be unblocked
@@ -11,14 +12,12 @@ app.use(express.static('public'))
 app.use(express.json())
 
 //API Routes
-
 // GET /api/notes should read the db.json file and return all saved notes as JSON.
 app.get('/api/notes', (req, res) => {
     res.json(db)
 })
 
-
-// POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
+//POST /api/notes recieves a new note to save on the request body and add it to db.json, then returns new note to the client.
 app.post('/api/notes', (req, res) => {
     const newNote = req.body
     newNote.id = uuidv4()
@@ -27,10 +26,9 @@ app.post('/api/notes', (req, res) => {
     res.json(db)
 })
 
+//DELETEs notes when the button is clicked by removing the note from db.json, saving and showing the updated database on the front end.
 app.delete('/api/notes/:id', (req, res) => {
-    // console.log(req.params.id)
     const newDb = db.filter((note) => note.id !== req.params.id)
-    console.log(newDb)
     fs.writeFileSync('./db/db.json', JSON.stringify(newDb))
     res.json(newDb)
     // res.sendFile(path.join(__dirname, './db/db.json'))
@@ -48,5 +46,6 @@ app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'notes.html'))
 })
 
+//App listens with front end on this port
 app.listen(PORT, () =>
     console.log(`App listening on ${PORT}`))
